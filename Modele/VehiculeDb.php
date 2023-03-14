ceece7f5c23ddb1918c3b5dbeb2cd9893ed4bb78
Michel MARQUETTE <marquettemichel73@gmail.com>
2023-03-14 15:50:17 +0100

    Rajout page détail vehicule

--- Modele/VehiculeDb.php
+++ Modele/VehiculeDb.php
@@ -3,13 +3,13 @@ require_once 'Modele.php';
 class VehiculeDb extends Modele {
 
     public function getVehicules() {
-        $sql = "SELECT id, modele, marque, km, etat, prixKm, disponible, image FROM vehicules";
+        $sql = "SELECT * FROM vehicules";
 		$resultats = $this->executerRequete($sql);
 		return $resultats;
     }
 
     public function getVehicule ($id) {
-		$sql = "SELECT modele, marque, km, etat, prixKm, disponible, image FROM vehicules WHERE id = ?"; 
+		$sql = "SELECT * FROM vehicules WHERE id = ?"; 
 		$resultat = $this->executerRequete($sql, array($id));
 		if ($resultat->rowCount() > 0)
 			return $resultat->fetch();
@@ -17,13 +17,13 @@ class VehiculeDb extends Modele {
 			return 0;
     }
 
-    public function modifVehiculeId ($id, $nom, $prenom, $mail, $telephone, $idCompte) {
-		$sql = "UPDATE vehicules SET modele=?, marque=?, km=?, etat=?, prixKm=?, disponible=?, image=? WHERE id = ?"; 
-		$this->executerRequete($sql, array ($nom, $prenom, $mail, $telephone, $idCompte, $id));
+    public function modifVehiculeId () {
+		$sql = "UPDATE vehicules SET modele=?, marque=?, km=?, couleur=?, etat=?, prixKm=?, disponible=?, image=?, moteur=?, carburant=?, transmission=?, chevaux=?, couple=?, vitesse=?, dimLong=?, dimLarg=?, dimH=?, poids=? WHERE id = ?"; 
+		$this->executerRequete($sql, array ());
 	}
 
-	public function ajoutVehicule($nom, $prenom, $mail, $telephone, $idCompte) {
-		$sql = 'INSERT INTO vehicules(modele, marque, km, etat, prixKm, disponible, image) VALUES (?, ?, ?, ?, ?, ?, ?)';
-		$this->executerRequete($sql, array($nom, $prenom, $mail, $telephone, $idCompte));
+	public function ajoutVehicule() {
+		$sql = 'INSERT INTO vehicules(modele marque, km, couleur, etat, prixKm, disponible, image, moteur, carburant, transmission, chevaux, couple, vitesse, dimLong, dimLarg, dimH, poids) VALUES (?, ?, ?, ?, ?, ?, ?)';
+		$this->executerRequete($sql, array());
 	}
 }
